984a4ea174453b36f0ad47b79e7bc53bd6e6a542
Jack Bracewell <jack@reasonable.engineering>
2021-07-13 15:13:57 +0100

    [2232] Add PGTA route id to code set

--- app/lib/dttp/code_sets/routes.rb
+++ app/lib/dttp/code_sets/routes.rb
@@ -7,6 +7,7 @@ module Dttp
         TRAINING_ROUTE_ENUMS[:assessment_only] => { entity_id: "99f435d5-a626-e711-80c8-0050568902d3" },
         TRAINING_ROUTE_ENUMS[:provider_led_postgrad] => { entity_id: "6189922e-acc2-e611-80be-00155d010316" },
         TRAINING_ROUTE_ENUMS[:early_years_undergrad] => { entity_id: "6b89922e-acc2-e611-80be-00155d010316" },
+        TRAINING_ROUTE_ENUMS[:pg_teaching_apprenticeship] => { entity_id: "cf9154ee-5678-e811-80f6-005056ac45bb" },
       }.freeze
     end
   end
