8cd1fb178bc68c60b3c2209c93ad3e5733e700ba
Stephan Ellenberger <stephan.ellenberger@digital.education.gov.uk>
2021-07-14 16:17:25 +0100

    [2215] Modified apply_trainee_data_form dynamically

--- app/forms/apply_trainee_data_form.rb
+++ app/forms/apply_trainee_data_form.rb
@@ -16,13 +16,17 @@ class ApplyTraineeDataForm
   validator :diversity, form: "Diversities::FormValidator"
   validator :degrees, form: "DegreesForm"
 
+  delegate :apply_application?, to: :trainee
+
   attr_accessor :mark_as_reviewed
 
-  def initialize(trainee:)
+  def initialize(trainee)
     @trainee = trainee
   end
 
   def save
+    return unless all_forms_valid?
+
     trainee.progress.personal_details = true
     trainee.progress.contact_details = true
     trainee.progress.diversity = true
@@ -30,8 +34,38 @@ class ApplyTraineeDataForm
     trainee.save!
   end
 
+  def all_forms_valid?
+    form_validators.each_value do |section|
+      return false unless form(section).valid?
+    end
+  end
+
+  def form(section)
+    section[:form].constantize.new(trainee)
+  end
+
   def progress_status(progress_key)
     progress_service(progress_key).status.parameterize(separator: "_").to_sym
+  end
+
+  def progress
+    form_progress = true
+    form_validators.each_key do |section_key|
+      progress = trainee.progress.public_send(section_key)
+      case progress
+      when false
+        form_progress = false
+      end
+    end
+    form_progress
+  end
+
+  def fields
+    fields = {}
+    form_validators.each_value do |section|
+      fields.merge!(form(section).fields)
+    end
+    fields
   end
 
   def display_type(section_key)
